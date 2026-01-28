6a5761c624fbfc84131fcfaac84347dbd27404f8
louyuanyang.ryuk <louyuanyang.ryuk@bytedance.com>
2026-01-28 20:22:28 +0800

    feat: init

--- database/db.go
+++ database/db.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"log"
-	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,13 +9,10 @@ import (
 
 var DB *gorm.DB
 
-func Connect() {
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
-		log.Fatal("DATABASE_URL environment variable is not set")
-	}
+const DSN = "doadmin:AVNS_1SUWMwvNP7bhHjPYpC1@tcp(db-mysql-sgp1-11646-do-user-6185766-0.m.db.ondigitalocean.com:25060)/say_right?tls=do&parseTime=true&charset=utf8mb4"
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+func Connect() {
+	db, err := gorm.Open(mysql.Open(DSN), &gorm.Config{
 		PrepareStmt: true,
 	})
 	if err != nil {
